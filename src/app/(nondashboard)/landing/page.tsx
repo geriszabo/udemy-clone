4b6bea8b370bdfd327bf4a9cce4652976a72119c
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-27 19:45:31 +0100

    chore: added landing

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useCarousel } from "@/hooks/useCarousel";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+
+const Landing = () => {
+  const currentImage = useCarousel({ totalImages: 3 });
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="landing"
+    >
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="landing__hero"
+      >
+        <div className="landing__hero-content">
+          <h1 className="landing__title">Courses</h1>
+          <p className="landing__description">
+            This is the list of courses you can enroll in.
+            <br />
+            courses when you need them and want them
+          </p>
+          <div className="landing__cta">
+            <Link href="/search">
+              <button className="landing__cta-button">
+                Search for Courses
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="landing__hero-images">
+          {["/hero1.jpg", "/hero2.jpg", "/hero3.jpg"].map((image, index) => {
+            return (
+              <Image
+                key={image}
+                src={image}
+                alt={`Hero banner ${index + 1}`}
+                fill
+                priority={index === currentImage}
+                sizes="(max-width:768p) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className={`landing__hero-image ${
+                  index === currentImage ? "landing__hero-image--active" : ""
+                }`}
+              />
+            );
+          })}
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ amount: 0.3, once: true }}
+        className="landing__featured"
+      >
+        <h2 className="landing__featured-title">Featured Courses</h2>
+        <p className="landing__featured-description">
+          From beginner to advanced, in all industries, we have the right
+          courses just for you and preparing your entire journey for learning
+          and making it the most.
+        </p>
+        <div className="landing__tags">
+          {[
+            "UI/UX",
+            "enterprise IT",
+            "react nextjs",
+            "frontend",
+            "backend",
+          ].map((tag, index) => (
+            <span key={index} className="landing__tag">
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="landing__courses">
+            {/* Courses will be here imo */}
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Landing;
