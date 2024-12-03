631e88d92be2c0ce18ade654ffadb98ddefc8a91
Jetigeri <szabo.gergo87j@gmail.com>
2024-12-03 18:04:33 +0100

    feat: signin component

---
+++ client/src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="auth-layout">
+      <main className="auth-layout__main">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ client/src/components/SignIn.tsx
@@ -0,0 +1,31 @@
+import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import React from "react";
+
+export const SignInComponent = () => {
+  return (
+    <SignIn
+      appearance={{
+        baseTheme: dark,
+        elements: {
+          rootBox: "flex justify-center items-center py-5",
+          cardBox: "shadow-none",
+          card: "bg-customgreys-secondarybg w-full sahdow-none",
+          formFieldLabel: "text-white-50 font-normal",
+          footer: {
+            background: "#25262F",
+            padding: "0rem 2.5rem",
+            "& > div > div:nth-child(1)": {
+              background: "#25262F",
+            },
+          },
+          formButtonPrimary:
+            "bg-primary-700 text-white-100 hover:bg-primary-600 !shadow-none",
+          formFieldInput:
+            "bg-customgreys-primartybg text-white-50 !shadow-none",
+          footerActionLink: "text-primary-750 hover:text-primarty-600",
+        },
+      }}
+    />
+  );
+};
