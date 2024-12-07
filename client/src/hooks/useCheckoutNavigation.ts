2e92ad324c91531382a3341284f8416ea426bf4f
Jetigeri <szabo.gergo87j@gmail.com>
2024-12-07 19:05:50 +0100

    chore: made client component

--- client/src/hooks/useCheckoutNavigation.ts
+++ client/src/hooks/useCheckoutNavigation.ts
@@ -1,28 +1,34 @@
-import { useUser } from '@clerk/nextjs'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useCallback, useEffect } from 'react'
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
 
 export const useCheckoutNavigation = () => {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const {isLoaded, isSignedIn} = useUser()
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { isLoaded, isSignedIn } = useUser();
 
-  const courseId = searchParams.get("id") ?? ""
-  const checkoutStep = parseInt(searchParams.get("step") ?? "1", 10)
+  const courseId = searchParams.get("id") ?? "";
+  const checkoutStep = parseInt(searchParams.get("step") ?? "1", 10);
 
-  const navigateToStep = useCallback((step: number) => {
-    const newStep = Math.min(Math.max(1, step), 3)
-    const showSignUp = String(isSignedIn)
+  const navigateToStep = useCallback(
+    (step: number) => {
+      const newStep = Math.min(Math.max(1, step), 3);
+      const showSignUp = String(isSignedIn);
 
-    router.push(`/checkout?step=${newStep}&id=${courseId}&showSignUp=${showSignUp}`)
-  }, [courseId, isSignedIn, router])
+      router.push(
+        `/checkout?step=${newStep}&id=${courseId}&showSignUp=${showSignUp}`
+      );
+    },
+    [courseId, isSignedIn, router]
+  );
 
   useEffect(() => {
-    if(isLoaded && !isSignedIn && checkoutStep > 1) {
-        navigateToStep(1)
+    if (isLoaded && !isSignedIn && checkoutStep > 1) {
+      navigateToStep(1);
     }
-  }, [checkoutStep, isLoaded, isSignedIn, navigateToStep])
+  }, [checkoutStep, isLoaded, isSignedIn, navigateToStep]);
 
-
-  return {checkoutStep, navigateToStep}
-}
+  return { checkoutStep, navigateToStep };
+};
