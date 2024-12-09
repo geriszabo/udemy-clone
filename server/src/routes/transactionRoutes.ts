7916207ab76ab10900ed0848907889f3f566a4c1
Jetigeri <szabo.gergo87j@gmail.com>
2024-12-09 15:27:42 +0100

    chore: created listtransactions path in router

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { createStripePaymentIntent, createTransaction } from "../controllers/transactionController"
+import { createStripePaymentIntent, createTransaction, listTransactions } from "../controllers/transactionController"
 
 const router = express.Router()
 
 router.post("/stripe/payment-intent", createStripePaymentIntent)
+router.get("/", listTransactions)
 router.post("/", createTransaction)
 
 export default router
