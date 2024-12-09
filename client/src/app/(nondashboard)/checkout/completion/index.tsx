f4b21e95dcd1008c520a21139699c15e9e886566
Jetigeri <szabo.gergo87j@gmail.com>
2024-12-09 14:39:23 +0100

    feat: checkout completed page

---
+++ client/src/app/(nondashboard)/checkout/completion/index.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Check } from "lucide-react";
+import Link from "next/link";
+
+const CompletionPage = () => {
+  return (
+    <div>
+      <div className="completion__content">
+        <div className="completion__icon">
+          <Check className="w-16 h-16" />
+        </div>
+        <h1 className="completion__title">COMPLETED</h1>
+        <p className="completion__message">
+          🎉 You have made a course purchase successfully! 🎉
+        </p>
+      </div>
+      <div className="completion__support">
+        <p>
+          Need help? Contact our{" "}
+          <Button variant="link" asChild className="p-0 m-0 text-primary-700">
+            <a href="mailto:support@example.com">customer support</a>
+          </Button>
+          .
+        </p>
+      </div>
+      <Button className="completion__action">
+        <Link href="user/courses" scroll={false}>
+          Go to Courses
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default CompletionPage;
