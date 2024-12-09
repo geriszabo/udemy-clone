868d1f197b76563803c03375a9e0de3dbfee6985
Jetigeri <szabo.gergo87j@gmail.com>
2024-12-09 21:58:36 +0100

    chore: created course crud controllers

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Course from "../models/courseModel";
+import { v4 as uuid } from "uuid";
+import { getAuth } from "@clerk/express";
 
 export const listCourses = async (
   req: Request,
@@ -19,16 +21,140 @@ export const listCourses = async (
   }
 };
 
-export const getCourse = async(req: Request, res: Response): Promise<void> => {
-  const {courseId} = req.params
-try {
-  const course = await Course.get(courseId)
-  if(!course) {
-    res.status(404).json({message: "Course not found", data: []})
-    return
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+  const { courseId } = req.params;
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found", data: [] });
+      return;
+    }
+    res.status(200).json({
+      message: `Course with id ${courseId} retrieved successfully`,
+      data: course,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving course", error });
   }
-  res.status(200).json({message: `Course with id ${courseId} retrieved successfully`, data: course})
-} catch (error) {
-  res.status(500).json({message: "Error retrieving course", error})
-}
-}
+};
+
+export const createCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { teacherId, teacherName } = req.body;
+    if (!teacherId || !teacherName) {
+      res.status(400).json({ message: "Teadcher Id and name are required" });
+    }
+
+    const newCourse = new Course({
+      courseId: uuid(),
+      teacherId,
+      teacherName,
+      title: "Untitled Course",
+      description: "",
+      category: "Uncategorized",
+      image: "",
+      price: 0,
+      level: "Beginner",
+      status: "Draft",
+      sections: [],
+      enrollments: [],
+    });
+
+    await newCourse.save();
+
+    res.status(200).json({
+      message: `Course created successfully`,
+      data: newCourse,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating course", error });
+  }
+};
+
+export const updateCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { courseId } = req.params;
+  const updateData = { ...req.body };
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: `Course with id ${courseId} not found` });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res.status(403).json({ message: "Not authorized to update this course" });
+      return;
+    }
+
+    if (updateData.price) {
+      const price = parseInt(updateData.price);
+      if (isNaN(price)) {
+        res.status(400).json({
+          message: "Invalid price format",
+          error: "Price must be a valid number",
+        });
+        return;
+      }
+      updateData.price = price * 100;
+    }
+
+    if (updateData.sections) {
+      const sectionsData =
+        typeof updateData.sections === "string"
+          ? JSON.parse(updateData.sections)
+          : updateData.sections;
+
+      updateData.sections = sectionsData.map((section: any) => ({
+        ...section,
+        sectionId: section.sectionId || uuid(),
+        chapters: section.chapters.map((chapter: any) => ({
+          ...chapter,
+          chapterId: chapter.chapterId || uuid(),
+        })),
+      }));
+    }
+
+    Object.assign(course, updateData);
+    await course.save();
+
+    res
+      .status(200)
+      .json({ message: "Course updated successfully", data: course });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating course", error });
+  }
+};
+
+export const deleteCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { courseId } = req.params;
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: `Course with id ${courseId} not found` });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res.status(403).json({ message: "Not authorized to delete this course" });
+      return;
+    }
+
+    await Course.delete(courseId);
+    res.status(200).json({ message: "Course deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting course", error });
+  }
+};
