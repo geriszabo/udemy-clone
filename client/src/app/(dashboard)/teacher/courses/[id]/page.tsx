8c7824122482fa035ee86b392e122c78c7ee59b8
Jetigeri <szabo.gergo87j@gmail.com>
2025-01-04 19:49:06 +0100

    chore: onsubmit video upload

--- client/src/app/(dashboard)/teacher/courses/[id]/page.tsx
+++ client/src/app/(dashboard)/teacher/courses/[id]/page.tsx
@@ -9,11 +9,13 @@ import { courseSchema } from "@/lib/schemas";
 import {
   centsToDollars,
   createCourseFormData,
+  uploadAllVideos,
 } from "@/lib/utils";
 import { openSectionModal, setSections } from "@/state";
 import {
   useGetCourseQuery,
   useUpdateCourseMutation,
+  useGetUploadVideoUrlMutation
 } from "@/state/api";
 import { useAppDispatch, useAppSelector } from "@/state/redux";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -30,6 +32,7 @@ const CourseEditor = () => {
   const id = params.id as string;
   const { data: course, isLoading, refetch } = useGetCourseQuery(id);
   const [updateCourse] = useUpdateCourseMutation();
+  const [getUploadVideoUrl] = useGetUploadVideoUrlMutation()
 
   const dispatch = useAppDispatch();
   const { sections } = useAppSelector((state) => state.global.courseEditor);
@@ -60,9 +63,13 @@ const CourseEditor = () => {
 
   const onSubmit = async (data: CourseFormData) => {
     try {
-    
+      const updatedSections = await uploadAllVideos(
+        sections,
+        id,
+        getUploadVideoUrl
+      );
 
-      const formData = createCourseFormData(data, sections);
+      const formData = createCourseFormData(data, updatedSections);
 
       await updateCourse({
         courseId: id,
