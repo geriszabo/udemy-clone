9d8f3732d02d8c27a3f79de3bfaba8ac7173d905
Jetigeri <szabo.gergo87j@gmail.com>
2025-01-04 19:38:32 +0100

    chore: added videoupload route

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import multer from "multer"
-import { createCourse, deleteCourse, getCourse, listCourses, updateCourse } from "../controllers/courseController"
+import { createCourse, deleteCourse, getCourse, listCourses, updateCourse, getUploadVideoUrl } from "../controllers/courseController"
 import { requireAuth } from "@clerk/express"
 
 const router = express.Router()
@@ -10,6 +10,12 @@ router.get("/", listCourses)
 router.post("/", requireAuth(), createCourse)
 router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse )
 router.delete("/:courseId", requireAuth(), deleteCourse)
+
+router.post(
+    "/:courseId/sections/:sectionId/chapters/:chapterId/get-upload-url",
+    requireAuth(),
+    getUploadVideoUrl
+  );
 
 router.get("/:courseId", getCourse)
 
